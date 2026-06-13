267be480bc4e59af2657edd21fc505934bdc1074
Fernando Arias <fernan.fpinformatica@gmail.com>
2026-06-13 20:41:24 +0200

    feat: configure Jest (#14)
    * refactor: bump `jest`, `ts-jest` and `@types/jest` versions
    
    * feat: migrate Jest configuration to TypeScript and configure `ts-jest` transforms
    
    * refactor: create new `tsconfig.build.json` optimized for build
    
    * feat: include "tests" folder and `jest.config.ts` in base tsconfig
    
    * feat: include add `jest` and `express` types in base tsconfig

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from 'jest';
+import { createDefaultPreset } from "ts-jest";
+
+const tsJestTransformCfg = createDefaultPreset().transform;
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/tests'],
+  testMatch: ['**/*.test.ts'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  collectCoverageFrom: [
+    'src/**/*.ts',
+    '!src/**/*.d.ts'
+  ],
+  transform: {
+    ...tsJestTransformCfg,
+  },
+};
+
+export default config;
