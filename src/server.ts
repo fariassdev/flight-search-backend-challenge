7339af6cab457367154027ef94daa2f44b8ea4e4
Fernando Arias <fernan.fpinformatica@gmail.com>
2026-06-17 17:14:37 +0200

    chore: fix linter issues after executing `npm run lint:fix`

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
 import { getDistanceBetweenAirports } from './modules/airport/airport.service';
 
 const app = express();
