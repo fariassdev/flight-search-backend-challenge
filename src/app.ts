b6e27b1c828c0522e71a62fa891b5b409665fa84
Fernando Arias <fernan.fpinformatica@gmail.com>
2026-06-18 21:21:26 +0200

    feat: use cors middleware in app

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import cors from 'cors';
 import express from 'express';
 import helmet from 'helmet';
 import { flightRoutes } from './modules/flight/flight.routes';
@@ -7,6 +8,7 @@ export function createApp() {
   const app = express();
 
   app.use(helmet());
+  app.use(cors());
 
   app.use('/api/flights', flightRoutes);
   app.use(errorHandler);
