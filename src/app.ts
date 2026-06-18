20a291ea6d674a2c68d65a2c372475dac64a21a5
Fernando Arias <fernan.fpinformatica@gmail.com>
2026-06-18 21:17:53 +0200

    feat: use helmet middleware in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import helmet from 'helmet';
 import { flightRoutes } from './modules/flight/flight.routes';
 import { errorHandler } from './shared/middleware/errorHandler';
 
 export function createApp() {
   const app = express();
+
+  app.use(helmet());
+
   app.use('/api/flights', flightRoutes);
   app.use(errorHandler);
   return app;
